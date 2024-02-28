11b018d4d70b3387316745b0f4a28a2f7e5723d9
Pedro Henrique <pedro.henrique.martins404@gmail.com>
2024-02-28 13:23:43 -0300

    fix: :truck: Rework Structure JS for new API

--- src/assents/js/login.js
+++ src/assents/js/login.js
@@ -1,45 +1,60 @@
 const alertSpan = document.querySelector('.login__label.alert');
 const button = document.querySelector('.login__label.btn .submit');
 
-if(localStorage.getItem('username') && localStorage.getItem('password')){
+if (localStorage.getItem('login') && localStorage.getItem('password')) {
     window.location.href = 'task.html';
 }
 
-const authenticatedRedirect = async (data,username, password) => {
+const authenticatedRedirect = async (data, login, password) => {
     if (data['status']) {
-        localStorage.setItem('username', username);
+        localStorage.setItem('login', login);
         localStorage.setItem('password', password);
-        localStorage.setItem('name', data['name']);
-        localStorage.setItem('task', '');
+        localStorage.setItem('name', data['profile']['name']);
+        localStorage.setItem('tasks', '');
 
+        console.log(data)
         window.location.href = 'task.html';
     } else {
-        alertSpan.classList.add('active')
+        alertSpan.classList.add('active');
     }
-}
+};
 
-const fetchAPI = async (username, password) => {
+const fetchAPI = async (login, password) => {
     try {
-        // const APIResponse = await fetch(`http://127.0.0.1:8000/ub/perfil/${username}&${password}`);
-        const APIResponse = await fetch(`https://ub-task-api.vercel.app/ub/perfil/${username}&${password}`);
-        
-        if (APIResponse.status == 200){
+        // const url = 'http://127.0.0.1:3000/ub/profile';
+        const url = 'https://ub-task-api.vercel.app/ub/profile';
+        const credentials = { login: login, password: password };
+
+        const APIResponse = await fetch(url, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(credentials)
+        });
+
+        if (APIResponse.status === 200) {
             const data = await APIResponse.json();
             return data;
+        } else {
+            return { status: false };
         }
-    } catch {
-        return {'status':false};
+    } catch (error) {
+        console.log('Erro:', error);
+        return { status: false };
     }
-}
+};
 
-document.getElementById('loginForm').addEventListener('submit', async function(event) {
+document.getElementById('loginForm').addEventListener('submit', async function (event) {
     event.preventDefault();
 
-    alertSpan.classList.remove('active')
-    button.disabled = true
-    const username = document.getElementById('username').value;
+    alertSpan.classList.remove('active');
+    button.disabled = true;
+
+    const login = document.getElementById('login').value;
     const password = document.getElementById('password').value;
-    
-    authenticatedRedirect(await fetchAPI(username, password),username, password);
-    button.disabled = false
+
+    authenticatedRedirect(await fetchAPI(login, password), login, password);
+
+    button.disabled = false;
 });
